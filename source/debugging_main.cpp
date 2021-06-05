c909cb361d2eeecd79d2a1df0da593239562fc4a
Hayden Smith <hayden.smith@unsw.edu.au>
2021-06-06 02:14:33 +1000

    Update debugging_main.cpp

--- source/debugging_main.cpp
+++ source/debugging_main.cpp
@@ -7,7 +7,7 @@
 //  frameworks might be overwhelming for some.
 
 auto main() -> int {
-    auto const english_lexicon = word_ladder::read_lexicon("./english.txt");
+    auto const english_lexicon = word_ladder::read_lexicon("./test/word_ladder/english.txt");
 	auto const ladders = word_ladder::generate("fly", "sky", english_lexicon);
 	// debug here
 }
